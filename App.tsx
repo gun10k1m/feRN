a161790c6f387789ddaf03b83de387af2a84708c
Koala Park <koala@10k1m.com>
2025-01-13 11:18:39 +0900

    상단 탭 네비게이션

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
 import {SafeAreaView, StyleSheet} from 'react-native';
 import SettingsScreen from './screens/SettingScreen';
 import HomeScreen from './screens/HomeScreen';
@@ -10,54 +10,52 @@ import ProfileScreen from './screens/ProfileScreen';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
 const Stack = createNativeStackNavigator();
-const Tab = createBottomTabNavigator();
-
-// 홈 스택 네비게이터
-function HomeStack() {
-  return (
-    <Stack.Navigator>
-      <Stack.Screen
-        name="Home"
-        component={HomeScreen}
-        options={{headerShown: true}}
-      />
-      <Stack.Screen name="Detail" component={DetailScreen} />
-    </Stack.Navigator>
-  );
-}
+const Tab = createMaterialTopTabNavigator();
 
 //렌더링 중에 새로운 컴포넌트를 정의하면 생기는 경고 해결
 const getTabBarIcon =
   (name: string) =>
-  ({color, size}: {color: string; size: number}) =>
-    <Icon name={name} color={color} size={size} />;
+  ({color}: {focused: boolean; color: string}) =>
+    <Icon name={name} color={color} size={24} />;
 
 function App(): React.JSX.Element {
   return (
     <SafeAreaView style={styles.container}>
       <NavigationContainer>
-        <Tab.Navigator>
-          <Tab.Screen
-            name="Home"
-            component={HomeStack}
-            options={{
-              headerShown: false,
-              tabBarIcon: getTabBarIcon('home'),
-            }}
+        <Stack.Navigator>
+          <Stack.Screen
+            name="MainTabs"
+            component={MainTabs}
+            options={{title: 'Main'}}
           />
-          <Tab.Screen
-            name="Profile"
-            options={{tabBarIcon: getTabBarIcon('person')}}
-            component={ProfileScreen}
-          />
-          <Tab.Screen
-            name="Settings"
-            options={{tabBarIcon: getTabBarIcon('settings')}}
-            component={SettingsScreen}
-          />
-        </Tab.Navigator>
+          <Stack.Screen name="Detail" component={DetailScreen} />
+        </Stack.Navigator>
       </NavigationContainer>
     </SafeAreaView>
+  );
+}
+
+function MainTabs() {
+  return (
+    <Tab.Navigator screenOptions={{tabBarShowLabel: false}}>
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          tabBarIcon: getTabBarIcon('home'),
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        options={{tabBarIcon: getTabBarIcon('person')}}
+        component={ProfileScreen}
+      />
+      <Tab.Screen
+        name="Settings"
+        options={{tabBarIcon: getTabBarIcon('settings')}}
+        component={SettingsScreen}
+      />
+    </Tab.Navigator>
   );
 }
 
